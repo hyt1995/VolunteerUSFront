f39e16b0af2df8e2c4ecf1e4186f00737abd9ca6
주혜민 <>
2022-05-22 17:10:44 +0900

    ADD: chart

--- src/component/organisms/GroupView.tsx
+++ src/component/organisms/GroupView.tsx
@@ -1,7 +1,32 @@
+import { useState, useEffect } from 'react';
 import styled, { css } from 'styled-components';
 import { FlexBox, SubTitle, Input } from '../atoms';
 import Image from 'next/image';
+import { Bar, Pie } from 'react-chartjs-2';
+import {
+    Chart as ChartJS,
+    CategoryScale,
+    LinearScale,
+    BarElement,
+    LineElement,
+    Title,
+    Tooltip,
+    Legend,
+    PointElement,
+    ArcElement
+} from 'chart.js';
 
+ChartJS.register(
+    CategoryScale,
+    LinearScale,
+    BarElement,
+    PointElement, //요기
+    LineElement,
+    Title,
+    Tooltip,
+    Legend,
+    ArcElement
+);
 const TopArea = styled.div<any>`
     padding: ${({ theme }) => theme.space.md};
     background: ${({ theme }) => theme.color.bg};
@@ -65,15 +90,170 @@ const InfoArea = styled.div<any>`
     .content {
         padding: ${({ theme }) => theme.space.sm};
     }
-
     ${(props) =>
         props.bg &&
         css`
             background: ${({ theme }) => theme.color.bg};
         `}
 `;
+const Chart = styled.div<any>`
+    width: 320px;
+    height: 337px;
+    margin: 0 auto;
+    box-shadow: 0px 0px 25px rgba(0, 0, 0, 0.15);
+    border-radius: 12px;
+    background: #fff;
 
+    .chart-canvas {
+        width: 100% !important;
+        height: 100% !important;
+        filter: blur(5px);
+        -webkit-filter: blur(5px);
+
+        &:hover {
+            filter: blur(0);
+            -webkit-filter: blur(0);
+        }
+    }
+`;
 const edit = true;
+const data = {
+    labels: ['10대', '20~24', '24~29', '30~34', '34~39', '40대~'],
+    datasets: [
+        {
+            type: 'bar',
+            label: '',
+            backgroundColor: ['#DBDFE5', '#147AD6', '#DBDFE5', '#DBDFE5', '#DBDFE5', '#DBDFE5'],
+            data: [1, 10, 3, 4, 5, 6],
+            borderRadius: 4
+        }
+    ]
+};
+const data2 = {
+    labels: ['남성', '여성'],
+    datasets: [
+        {
+            type: 'pie',
+            label: '',
+            backgroundColor: ['#5B61FF', '#EDBAFF'],
+            data: [60, 40]
+        }
+    ]
+};
+const options = {
+    spanGaps: true,
+    responsive: true,
+    plugins: {
+        axis: {
+            y: {
+                show: false
+            }
+        },
+        legend: {
+            display: false
+        },
+        title: {
+            display: true,
+            text: '20대가 가장 많아요!',
+            color: '#344052',
+            font: {
+                weight: 'bold',
+                size: 12
+            },
+            padding: {
+                bottom: 30
+            }
+        },
+        tooltip: {
+            enabled: false
+        }
+    },
+    scales: {
+        x: {
+            grid: {
+                display: false
+            },
+            position: 'bottom'
+        },
+        y: {
+            display: false,
+            grid: {
+                display: false
+            }
+        },
+        yAxes: [
+            {
+                ticks: {
+                    display: false
+                }
+            }
+        ]
+    }
+};
+const options2 = {
+    spanGaps: true,
+    responsive: true,
+    plugins: {
+        datalabels: {
+            color: 'white',
+            display: function (context) {
+                return context.dataset.data[context.dataIndex] > 15;
+            },
+            font: {
+                weight: 'bold'
+            },
+            formatter: Math.round
+        },
+
+        axis: {
+            x: {
+                show: false
+            },
+            y: {
+                show: false
+            }
+        },
+        legend: {
+            display: false
+        },
+        title: {
+            display: true,
+            text: '남성이 더 많아요!',
+            color: '#344052',
+            font: {
+                weight: 'bold',
+                size: 12
+            },
+            padding: {
+                bottom: 30
+            }
+        },
+        tooltip: {
+            enabled: false
+        }
+    },
+    scales: {
+        x: {
+            display: false,
+            grid: {
+                display: false
+            }
+        },
+        y: {
+            display: false,
+            grid: {
+                display: false
+            }
+        },
+        yAxes: [
+            {
+                ticks: {
+                    display: false
+                }
+            }
+        ]
+    }
+};
 
 const GroupView = () => {
     return (
@@ -140,7 +320,30 @@ const GroupView = () => {
                 <FlexBox column>
                     <SubTitle fontSize="14px">그룹 멤버 정보</SubTitle>
                     <div className="content">
-                        <img src="" alt="" />
+                        <Chart>
+                            {data.datasets ? (
+                                <Bar
+                                    className="chart-canvas"
+                                    options={options}
+                                    data={data}
+                                    style={{ width: '100%', height: '100%', padding: '30px' }}
+                                />
+                            ) : (
+                                ''
+                            )}
+                        </Chart>
+                        <Chart style={{ margin: '30px auto 0' }}>
+                            {data.datasets ? (
+                                <Pie
+                                    className="chart-canvas"
+                                    options={options2}
+                                    data={data2}
+                                    style={{ width: '100%', height: '100%', padding: '30px' }}
+                                />
+                            ) : (
+                                ''
+                            )}
+                        </Chart>
                     </div>
                 </FlexBox>
             </InfoArea>
